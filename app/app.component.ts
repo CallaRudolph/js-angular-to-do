75f6d5defe194fd68d9bf04011ce28b901c57683
Calla Rudolph and jacob ruleaux <calla@>
2017-08-15 09:57:21 -0700

    create task-list component and task.model

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,22 +7,23 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit$</button></li>
-    </ul>
+
+    <task-list></task-list>
     <hr>
-    <div *ngIf="selectedTask">
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
-      <h3>Edit Task</h3>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-      <label>Enter Task Priority (1-3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-      <button (click)="finishedEditing()">Done</button>
+    <div>
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Finished Editing</button>
+      </div>
     </div>
   </div>
   `
@@ -33,40 +35,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course.', 3),
-    new Task('Begin brainstorming possible JavaScript group projects.', 2),
-    new Task('Add README file to last few Angular repos on GitHub.', 2) ];
   selectedTask = null;
 
   editTask(clickedTask){
     this.selectedTask = clickedTask;
   }
 
-  isDone(clickedTask: Task){
-    if(clickedTask.done === true){
-      alert("Done");
-    } else {
-      alert("Not done");
-    }
-  }
-
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2){
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
   finishedEditing(){
     this.selectedTask = null;
   }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) { }
 }
